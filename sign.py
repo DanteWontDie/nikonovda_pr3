18169c2331b894b757f92ba088ad7c790d3bd961
DanteWontDie <dinikonov@mail.ru>
2026-05-24 02:01:10 +0300

    вычисление подписи

---
+++ sign.py
@@ -0,0 +1,35 @@
+# функция создания ЭП
+
+import random
+from arithmetic_of_pints import scalar_mult
+
+def sign(h, d, params):
+    p = params["p"]
+    a = params["a"]
+    q = params["q"]
+    xP = params["xP"]
+    yP = params["yP"]
+
+    # вычисляем целое число a из хеша вектора h 
+    alpha = int.from_bytes(h, "big")
+    e = alpha % q
+    if e == 0:
+        e = 1
+    
+    while True:
+        # генерируем случайное k которое 0 < k < q
+        k = random.randint(1, q - 1)
+
+        # вычисляем точку С = kP и определяем r = xc mod q
+        C = scalar_mult(k, (xP, yP), p, a)
+        r = C[0] % q
+        if r == 0:
+            continue
+    
+        # вычисляем вторую компоненту s
+        s = (r * d + k * e) % q
+        if s == 0:
+            continue
+
+        # подпись
+        return r, s
